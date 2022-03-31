1000364c7a6f7e701b69ccc300b21a89b2af03d6
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-03-31 15:19:37 -0300

    Criaçao dos arquivos do Redux

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,12 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const USER_INITIAL_STATE = {
+  email: '',
+};
+
+const user = (state = USER_INITIAL_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default user;
