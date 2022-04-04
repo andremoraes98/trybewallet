59d451fd5ae0e79eb98746f67c5e18f45f6d4325
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-04-04 14:39:05 -0300

    Estilização do Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import crypto from '../images/crypto.png';
 
 class Header extends Component {
   render() {
     const { userEmail, totalValue } = this.props;
     return (
       <header>
-        <p>
-          Email:
-          <span data-testid="email-field">{userEmail}</span>
-        </p>
+        <img src={ crypto } alt="Crypto Wallet" />
+        <div className="userInfos">
+          <p>
+            Email:
+            <span data-testid="email-field">{userEmail}</span>
+          </p>
 
-        <p data-testid="total-field">
-          { totalValue }
-        </p>
+          <p data-testid="total-field">
+            { totalValue || 0 }
+          </p>
 
-        <p data-testid="header-currency-field">
-          BRL
-        </p>
+          <p data-testid="header-currency-field">
+            BRL
+          </p>
+        </div>
       </header>
     );
   }
